5ad850b29c24e4a9897320deab536bc45fd1b45f
amyhynes <amy.hynes@mail.mcgill.ca>
2020-05-03 17:46:52 -0400

    add comments

--- count_cells.py
+++ count_cells.py
@@ -18,28 +18,31 @@ if len(sys.argv) < 2 or  len(sys.argv) > 3:
     print(exception)
     sys.exit(0)
 
+#load image
 try:
 	img = io.imread(sys.argv[1])
 except:
 	print(exception)
 	sys.exit(0)
 
+#preprocess image and extract all possible positive patches
 label_image, patch_arr, patches = preprocess_iba1.preprocess(img)
 
+#extract features for each patch
 X_test = extract_features.extract_features(label_image, patch_arr).to_numpy()
 
+#use Decision Tree model to predict number of positive cells
 loaded_model = joblib.load('helpers/finalized_dt_model.sav')
 y_pred = loaded_model.predict(X_test)
 result = np.sum(y_pred)
 print("Number of Positive Iba1 cells: ", result)
 
-pos_patches = []
-i = 0
-for pred in y_pred:
-	if pred == 1:
-		pos_patches.append(patches[i])
-	i+=1
-
+#generate bbox image
 if len(sys.argv) > 2 and sys.argv[2] == '--img':
-	#generate bbox image
+	pos_patches = []
+	i = 0
+	for pred in y_pred:
+		if pred == 1:
+			pos_patches.append(patches[i])
+		i+=1
 	bbox.bbox_img_generator(img_as_float(img), pos_patches)
